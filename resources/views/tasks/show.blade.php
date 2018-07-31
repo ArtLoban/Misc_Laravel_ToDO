d04845f06090f86702e24ca9d0c650c4a1286bf6
Artem Loban <tymanrock@gmail.com>
2018-07-31 19:47:52 +0300

    Fixing output text line breakes

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -7,7 +7,9 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-1">
             <p class="task-item-title">{{$task->title }}</p>
-            <p>{{$task->description}}</p>
+{{--            <p>{{$task->description}}</p>--}}
+{{--            <p>{!! nl2br($task->description) !!}</p>--}}
+            <p>{!! nl2br(e($task->description)) !!}</p>
             <p class="created_at">( created {{$task->created_at->diffForHumans()}} )</p>
         </div>
     </div>
